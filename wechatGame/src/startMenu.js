faa4673883568d63ca6db8b9d491282128bd16f3
ttaccp <ttaccp@vip.qq.com>
2015-06-26 15:53:56 +0800

    修改动画间隔

--- wechatGame/src/startMenu.js
+++ wechatGame/src/startMenu.js
@@ -127,15 +127,15 @@ var StartMenu = cc.Layer.extend({
     },
     show: function(node, time){
     	node.setVisible(true);
-    	var fadeto = new cc.FadeTo(time || 0.2, 255);
+    	var fadeto = new cc.FadeTo(time || 0.5, 255);
         node.runAction(fadeto);
     },
     hide: function(node, time){
-    	var fadeto = new cc.FadeTo(time || 0.2, 0);
+    	var fadeto = new cc.FadeTo(time || 0.5, 0);
         node.runAction(fadeto);
         setTimeout(function(){
         	node.setVisible(false);
-        }, time || 0.2);
+        }, time || 0.5);
     }
 });
 
