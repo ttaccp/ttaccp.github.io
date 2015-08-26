f1734862ce748fac8fd62aaf46ae2a1d78b67a76
ttaccp <ttaccp@vip.qq.com>
2015-08-27 00:17:23 +0800

    更换视频

--- pengyouquan/js/persondetails.js
+++ pengyouquan/js/persondetails.js
@@ -3,7 +3,7 @@ setPersonDetails(selectedPersonCard);
 function setPersonDetails(data){
 	$('#step4_head').attr('src', data.cardHead);
 	$('#step4_name').html(data.cardName + '<i></i>');
-	$('#step4_nickname').html('昵称：' + data.cardNickName);
+	$('#step4_nickname').html(data.cardNickName);
 }
 
 $('#personBtn').click(function(){
